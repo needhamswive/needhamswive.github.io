36f7e310f6fd1c3319d8ccc7e063405fd2654e62
Zibing Zhang <zibing.zha@gmail.com>
2023-10-07 20:56:56 -0400

    Seeded random

--- assets/wrapped/2023/main.js
+++ assets/wrapped/2023/main.js
@@ -10,7 +10,7 @@ class Slide {
   }
 
   nextHexColor() {
-    return this.hexColors[Math.floor(Math.random() * this.hexColors.length)];
+    return this.hexColors[Math.floor(random() * this.hexColors.length)];
   }
 }
 
@@ -58,8 +58,10 @@ let activeSlideIndex;
 let hexTemplate;
 
 const athleteName = new URLSearchParams(window.location.search).get("athlete") || "jane-smith";
-const athletePath = location.origin + `/assets/wrapped/2023/athletes/${athleteName}.json`;
+const athletePath = window.location.origin + `/assets/wrapped/2023/athletes/${athleteName}.json`;
 let athlete = fetch(athletePath).then((response) => response.json());
+
+let random = mulberry32(cyrb128(athleteName)[0]);
 
 window.addEventListener("DOMContentLoaded", async () => {
   progressBar = document.getElementsByClassName("progress")[0];
@@ -197,21 +199,6 @@ function animateHexes() {
   }
 }
 
-// https://stackoverflow.com/a/19270021
-function getRandom(arr, n) {
-  var result = new Array(n),
-    len = arr.length,
-    taken = new Array(len);
-  if (n > len)
-    throw new RangeError("getRandom: more elements taken than available");
-  while (n--) {
-    var x = Math.floor(Math.random() * len);
-    result[n] = arr[x in taken ? taken[x] : x];
-    taken[x] = --len in taken ? taken[len] : len;
-  }
-  return result;
-}
-
 function preprocessAthlete(athlete) {
   athlete.slides.push({
     "name": "team-summary",
@@ -237,5 +224,48 @@ function processAthlete() {
       }
       placeholder.innerHTML = replacementValue;
     }
+  }
+}
+
+// https://stackoverflow.com/a/19270021
+function getRandom(arr, n) {
+  var result = new Array(n),
+    len = arr.length,
+    taken = new Array(len);
+  if (n > len)
+    throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+    var x = Math.floor(random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
+
+// https://stackoverflow.com/a/47593316
+function cyrb128(str) {
+  let h1 = 1779033703, h2 = 3144134277,
+      h3 = 1013904242, h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+  return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
+function mulberry32(a) {
+  return function() {
+    var t = a += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
   }
 }
