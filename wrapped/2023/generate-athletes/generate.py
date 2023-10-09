e3a0a1df8b2252bad27b6934047ac40720037da6
Zibing Zhang <zibing.zha@gmail.com>
2023-10-08 22:40:36 -0400

    Generate slides for all existing slides

--- wrapped/2023/generate-athletes/generate.py
+++ wrapped/2023/generate-athletes/generate.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 import csv
 import json
 from collections import OrderedDict
-from typing import Any
+from typing import Any, Callable
+
 
 class Table:
     def __init__(self, path: str, columns: set[str]) -> None:
@@ -18,8 +20,11 @@ class Table:
                     for column, index in column_to_indices.items()
                 }))
 
-    def get(self, first_last: str) -> Any:
-        return next(filter(lambda row: row.first_last == first_last, self.rows))
+    def filter(self, func: Callable[[TableRow], bool]) -> None:
+        self.rows = list(filter(func, self.rows))
+
+    def get_by_name(self, name: str) -> Any:
+        return list(filter(lambda row: row.first_last == name, self.rows))
 
 class TableRow:
     def __init__(self, properties: dict[str, Any]) -> None:
@@ -31,28 +36,24 @@ class TableRow:
     def __getattr__(self, _name: str) -> Any:
         return self._properties[_name]
 
-roster_table = Table("data/roster.tsv", {"first", "nickname", "last", "grade"})
-attendance_table = Table("data/attendance.tsv", {"diver", "first-last", "practice-count", "practice-percentage", "total-yardage"})
-swim_results_table = Table("data/swim-results.tsv", {"first-last", "event", "time", "formatted-time", "place", "points", "date", "meet", "sectionals-qualifier", "states-qualifier"})
-individual_dive_results_table = Table("data/individual-dive-results.tsv", {"first-last", "dive-order", "dive", "degree-of-difficulty", "total-score", "qualified", "date", "meet"})
-overall_dive_results_table = Table("data/overall-dive-results.tsv", {"first-last", "dive-format", "score", "place", "date", "meet", "sectionals-qualifier", "states-qualifier"})
 
 class Athlete:
-    def __init__(self, first: str, nickname: str, last: str) -> None:
+    def __init__(self, first: str, nickname: str, last: str, grade: int) -> None:
         self.first = first
         self.nickname = nickname
         self.last = last
+        self.grade = grade
 
     def __str__(self) -> str:
         return (
             "<"
-            f"Athlete name:\"{self.nickname if self.nickname else self.first} {self.last}\" "
+            f"Athlete name:\"{self.nickname if self.nickname else self.first} {self.last}\""
             f"attendance:({self.practice_count}, {self.practice_percentage}%)"
             ">"
         )
 
     @property
-    def first_last(self) -> str:
+    def name(self) -> str:
         return f"{self.first} {self.last}"
 
     @property
@@ -61,7 +62,11 @@ class Athlete:
 
     @property
     def file_name(self) -> str:
-        return self.display_name.lower().replace(" ", "-").replace("'", "") + ".json"
+        return self.name.lower().replace(" ", "-").replace("'", "") + ".json"
+
+    @property
+    def is_senior(self) -> bool:
+        return self.grade == 12
 
     @property
     def json(self) -> str:
@@ -92,20 +97,76 @@ class Athlete:
                 ])),
             ]))
 
+        if self.diver:
+            self.dives.sort(key=lambda dive: dive.score, reverse=True)
+            recorded_numbers = set()
+            top_per_number = []
+            for dive in self.dives:
+                if dive.number not in recorded_numbers:
+                    top_per_number.append(dive)
+                    recorded_numbers.add(dive.number)
+            replacements = OrderedDict()
+            for i, dive in enumerate(top_per_number[:5]):
+                replacements[f"dive-number-{i+1}"] = dive.number
+                replacements[f"dive-score-{i+1}"] = f"{dive.score:.2f}"
+            slides.append(OrderedDict([
+                ("name", "top-dives"),
+                ("replacements", replacements),
+            ]))
+
+        if self.is_senior:
+            slides.append(OrderedDict([
+                ("name", "goodbye-senior"),
+            ]))
+        else:
+            slides.append(OrderedDict([
+                ("name", "goodbye"),
+            ]))
+
         return json.dumps(OrderedDict([("slides", slides)]), indent=2)
 
-    def set_attendance(self, attendance_table):
-        attendance_row = attendance_table.get(self.first_last)
+    def set_attendance(self, attendance_table) -> None:
+        attendance_row = attendance_table.get_by_name(self.name)[0]
         self.diver = attendance_row.diver == "TRUE"
         self.practice_count = int(attendance_row.practice_count)
         self.practice_percentage = attendance_row.practice_percentage
         if not self.diver:
             self.total_yardage = attendance_row.total_yardage
 
-athletes = [Athlete(row.first, row.nickname, row.last) for row in roster_table.rows]
-athletes.sort(key=lambda athlete: athlete.last + athlete.first)
+    def set_individual_dive_results(self, individual_dive_results_table) -> None:
+        if not self.diver:
+            return
+        self.dives = list(map(IndividualDiveResult, individual_dive_results_table.get_by_name(self.name)))
 
-for athlete in athletes:
-    athlete.set_attendance(attendance_table)
-    with open("athletes/" + athlete.file_name, "w") as f:
-        f.write(athlete.json)
+
+class IndividualDiveResult:
+    def __init__(self, row: TableRow) -> None:
+        self.number = row.dive_number
+        self.dod = float(row.degree_of_difficulty)
+        self.score = float(row.total_score)
+
+    def __repr__(self) -> str:
+        return f"<IndividualDiveResult number:{self.number} dod:{self.dod} score:{self.score}>"
+
+
+def main() -> None:
+    roster_table = Table("data/roster.tsv", {"first", "nickname", "last", "grade"})
+    attendance_table = Table("data/attendance.tsv", {"diver", "first-last", "practice-count", "practice-percentage", "total-yardage"})
+    swim_results_table = Table("data/swim-results.tsv", {"first-last", "event", "time", "formatted-time", "place", "points", "date", "meet", "sectionals-qualifier", "states-qualifier"})
+    individual_dive_results_table = Table("data/individual-dive-results.tsv", {"first-last", "dive-order", "dive-number", "degree-of-difficulty", "ns", "total-score", "qualified", "date", "meet"})
+    overall_dive_results_table = Table("data/overall-dive-results.tsv", {"first-last", "dive-format", "score", "place", "date", "meet", "sectionals-qualifier", "states-qualifier"})
+
+    individual_dive_results_table.filter(lambda row: row.ns == "FALSE")
+
+    athletes = [Athlete(row.first, row.nickname, row.last, int(row.grade)) for row in roster_table.rows]
+    athletes.sort(key=lambda athlete: athlete.last + athlete.first)
+
+    for athlete in athletes:
+        athlete.set_attendance(attendance_table)
+        athlete.set_individual_dive_results(individual_dive_results_table)
+        with open("athletes/" + athlete.file_name, "w") as f:
+            f.write(athlete.json)
+
+
+if __name__ == "__main__":
+    main()
