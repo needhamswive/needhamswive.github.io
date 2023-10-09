059cb27f7102f003df9f9604ca6d0f101f97b1fb
Zibing Zhang <zibing.zha@gmail.com>
2023-10-08 21:21:08 -0400

    Attendance table

--- wrapped/2023/generate-athletes/generate.py
+++ wrapped/2023/generate-athletes/generate.py
@@ -26,7 +26,20 @@ class TableRow:
     def __getattr__(self, _name: str) -> Any:
         return self._properties[_name]
 
-roster_table = Table("data/roster.tsv", {"first", "nickname", "last", "grade"})
+roster = Table("data/roster.tsv", {"first", "nickname", "last", "grade"})
+attendance = Table("data/attendance.tsv", {"first-last", "practice-count", "practice-percentage"})
 swim_results = Table("data/swim-results.tsv", {"first-last", "event", "time", "formatted-time", "place", "points", "date", "meet", "sectionals-qualifier", "states-qualifier"})
 individual_dive_results = Table("data/individual-dive-results.tsv", {"first-last", "dive-order", "dive", "degree-of-difficulty", "total-score", "qualified", "date", "meet"})
 overall_dive_results = Table("data/overall-dive-results.tsv", {"first-last", "dive-format", "score", "place", "date", "meet", "sectionals-qualifier", "states-qualifier"})
+
+class Athlete:
+    def __init__(self, first: str, nickname: str, last: str) -> None:
+        self.first = first
+        self.nickname = nickname
+        self.last = last
+
+    def __str__(self) -> str:
+        return f"<Athlete name:\"{self.nickname if self.nickname else self.first} {self.last}\">"
+
+athletes = [Athlete(row.first, row.nickname, row.last) for row in roster.rows]
+athletes.sort(key=lambda athlete: athlete.last + athlete.first)
