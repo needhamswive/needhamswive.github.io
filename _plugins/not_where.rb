dfc3eb77f752be2631c3727455b424b3e94fd958
Zibing Zhang <zibing.zha@gmail.com>
2025-12-01 07:30:02 -0500

    Modify not_where filter

--- _plugins/not_where.rb
+++ _plugins/not_where.rb
@@ -1,22 +1,13 @@
 # https://github.com/Shopify/liquid/blob/e3dcc75ab533eb177cabf3df16ed27c1a21fc33c/lib/liquid/standardfilters.rb#L207-L225
 module Jekyll
   module NotWhereFilter
-    def not_where(input, property, target_value = nil)
+    def not_where(input, property, target_value)
       ary = input
 
       if ary == nil
         []
       elsif ary.empty?
         []
-      elsif target_value.nil?
-        ary.select do |item|
-          !item[property]
-        rescue TypeError
-          raise_property_error(property)
-        rescue NoMethodError
-          return nil unless item.respond_to?(:[])
-          raise
-        end
       else
         ary.select do |item|
           item[property] != target_value
